1fe69dca7bdf78ea064c4beb05f9d1dfa41a40d2
Mateusz Wójcik <mateusz-wojcik@hotmail.com>
2019-11-21 20:43:20 +0100

    Modified Api source

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,5 +1,5 @@
 const getWord = async () =>  {
-    const res = await fetch("http://puzzle.mead.io/puzzle")
+    const res = await fetch("//puzzle.mead.io/puzzle")
     const task = await res.json()
     return task.puzzle
 }
