7b236ba9e6b91094225aff5559e54aa8d64a2d68
Mateusz Wójcik <mateusz-wojcik@hotmail.com>
2019-11-21 20:38:29 +0100

    Add styles and style manipulation

--- scripts/app.js
+++ scripts/app.js
@@ -7,20 +7,27 @@ const statusDOM = document.querySelector("#status");
 // attemptsDOM.textContent = game1.attempts;
 
 window.addEventListener("keypress", (key) => {
+    key.preventDefault();
     if (!game1.attempts < 1) {
         const letter = String.fromCharCode(key.charCode);
         game1.makeGuess(letter);
         render()
-        game1.statusMessage;
+        // game1.statusMessage;
     } else {
         console.log("  .   " + game1.attempts)
     }
     statusDOM.textContent = game1.statusMessage;
 });
 const render = () => {
-    wordDOM.textContent = game1.guessLetter;
+    // wordDOM.textContent = game1.guessLetter;
+    wordDOM.innerHTML = '';
     attemptsDOM.textContent = game1.attempts;
     statusDOM.textContent = game1.statusMessage;
+    game1.guessLetter.split('').forEach(letter => {
+        const letterEl = document.createElement("span");
+        letterEl.textContent = letter;
+        wordDOM.appendChild(letterEl);
+    })
 }
 const startGame = async () => {
     const word = await getWord()
@@ -32,3 +39,9 @@ document.querySelector("#reset").addEventListener("click", startGame)
 startGame()
 
 // getWord().then(task => console.log('task.puzzle :', task))
+
+window.addEventListener("keydown",function (e) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) { 
+        e.preventDefault();
+    }
+})
